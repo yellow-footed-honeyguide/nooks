05b9730c7a17c3713488cd411347c612e3a7ab85
Sergey Veneckiy <Sergey_Veneckiy@epam.com>
2024-06-05 20:40:53 +0400

    fix: Resolve issue with non-functional -a option fix: Correct the behavior of the --save option enhance: Improve output messages of specific functions

--- handle_arguments.c
+++ handle_arguments.c
@@ -1,56 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 #include <getopt.h>
 #include "handle_arguments.h"
 #include "actions.h"
 
-/**
- * Handles command line arguments using the getopt_long library.
- * Supports options to save the current directory, show all saved spots, or navigate to a specific directory.
- * 
- * @param argc The argument count.
- * @param argv The argument vector.
- */
 void handle_arguments(int argc, char *argv[]) {
-    int opt;  // Variable to store the option character
-    char *directory_name = NULL;  // Pointer to store directory name if provided
-
+    int opt;
+    char *directory_name = NULL;
+    
     // Define long options
     static struct option long_options[] = {
-        {"save", optional_argument, 0, 's'},
+        {"save", required_argument, 0, 's'},
         {"all",  no_argument,       0, 'a'},
-        {"help",  no_argument,      0, 'h'},
+        {"help", no_argument,       0, 'h'},
         {0,      0,                 0,  0 }
     };
-
-    int long_index = 0;  // Index of the long option
-
-    // Loop through the command line options
-    while ((opt = getopt_long(argc, argv, "s:a:h", long_options, &long_index)) != -1) {
+    
+    int long_index = 0;
+    
+    // Parse command-line options
+    while ((opt = getopt_long(argc, argv, "s:ah", long_options, &long_index)) != -1) {
         switch (opt) {
-            case 's':  // Option to save the current directory
-                directory_name = optarg ? optarg : "default";  // Use provided name or default
+            case 's':
+                // Save current directory with provided name
+                directory_name = optarg;
                 save_current_directory(directory_name);
                 break;
-            case 'a':  // Option to show all saved spots
+            case 'a':
+                // Show all saved spots
                 show_all_spots();
                 break;
-
-            case 'h':  // Option to show all saved spots
+            case 'h':
+                // Print help information
                 print_help();
                 break;
-
-            default:  // Unrecognized option, print usage information
-                fprintf(stderr, "Usage: %s [-s|--save [directory_name]] [-a|--all] [directory_name]\n", argv[0]);
+            case '?':
+                // Handle unknown options and missing arguments
+                if (optopt == 's') {
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+                }
+                fprintf(stderr, "Usage: %s [-s|--save directory_name] [-a|--all] [-h|--help] [directory_name]\n", argv[0]);
                 exit(EXIT_FAILURE);
+            default:
+                abort();
         }
     }
-
-    // Check for non-option arguments (directory names)
+    
+    // Handle non-option arguments
     if (optind < argc) {
-        go_to_directory(argv[optind]);  // Go to the directory specified as a non-option argument
-    } else if (optind == 1) {  // No options or directory names provided
-        go_to_directory("default");  // Go to the default directory
+        // Go to the provided directory
+        go_to_directory(argv[optind]);
+    } else if (optind == 1) {
+        // Go to the default directory if no options or arguments provided
+        go_to_directory("default");
     }
 }
