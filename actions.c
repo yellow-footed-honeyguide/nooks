f8740fb4f84d51cdd190c745962fedd0afd99d3f
Sergey Veneckiy <Sergey_Veneckiy@epam.com>
2024-06-05 15:40:05 +0400

    fix: Resolve issue preventing multiple rewrites of default or custom spots

--- actions.c
+++ actions.c
@@ -8,22 +8,63 @@ void save_current_directory(const char *spot) {
     char *home_dir = getenv("HOME");
     char config_path[MAX_PATH];
     snprintf(config_path, sizeof(config_path), "%s%s", home_dir, CONFIG_FILE);
- 
-    FILE *file = fopen(config_path, "a");
+
+    char temp_path[MAX_PATH];
+    snprintf(temp_path, sizeof(temp_path), "%s.tmp", config_path);
+
+    FILE *file = fopen(config_path, "r");
+    FILE *temp_file = fopen(temp_path, "w");
+
     if (file == NULL) {
         perror("Error opening file");
         exit(1);
     }
- 
+
+    if (temp_file == NULL) {
+        perror("Error creating temporary file");
+        fclose(file);
+        exit(1);
+    }
+
     char current_dir[MAX_PATH];
     if (getcwd(current_dir, sizeof(current_dir)) == NULL) {
         perror("Error getting current directory");
         fclose(file);
+        fclose(temp_file);
         exit(1);
     }
- 
-    fprintf(file, "%s %s\n", spot, current_dir);
+
+    int spot_found = 0;
+    char line[MAX_PATH];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        char saved_spot[MAX_PATH];
+        sscanf(line, "%s", saved_spot);
+
+        if (strcmp(saved_spot, spot) == 0) {
+            fprintf(temp_file, "%s %s\n", spot, current_dir);
+            spot_found = 1;
+        } else {
+            fprintf(temp_file, "%s", line);
+        }
+    }
+
+    if (!spot_found) {
+        fprintf(temp_file, "%s %s\n", spot, current_dir);
+    }
+
     fclose(file);
+    fclose(temp_file);
+
+    if (remove(config_path) != 0) {
+        perror("Error deleting original file");
+        exit(1);
+    }
+
+    if (rename(temp_path, config_path) != 0) {
+        perror("Error renaming temporary file");
+        exit(1);
+    }
+
     printf("Current directory saved as spot '%s'\n", spot);
 }
 
